9ed754f05a5c1a319a88d02189540b61e7a7f2bf
Jasonhsu6 <35356776+Jasonhsu6@users.noreply.github.com>
2019-10-14 11:00:29 -0400

    django initial

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Xugao\Desktop\Python Training\Rental_management_sys\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
