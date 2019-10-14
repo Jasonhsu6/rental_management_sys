71b1d39bdd165668743436bb63cf85e3e2c9a86a
Jasonhsu6 <35356776+Jasonhsu6@users.noreply.github.com>
2019-10-14 15:51:33 -0400

    Rental model

--- equipment/models.py
+++ equipment/models.py
@@ -5,14 +5,15 @@ from django.db import models
 
 
 class Equipment(models.Model):
-    category = models.ForeignKey('Category', on_delete = models.CASCADE)
-    vendor = models.ForeignKey('Vendor', on_delete = models.CASCADE)
+    category = models.ForeignKey('Category', on_delete = models.SET_NULL, null=True)
+    vendor = models.ForeignKey('Vendor', on_delete = models.SET_NULL, null=True)
     name = models.CharField(max_length = 50)
-    serial_number = models.CharField(max_length = 6)
+    serial_number = models.CharField(max_length = 6, unique=True)
     available = models.BooleanField(default = True)
     day_rate = models.FloatField(default = 0)
     week_rate = models.FloatField(default = 0)
     month_rate = models.FloatField(default = 0)
+    retail_price = models.FloatField(default = 0)
     def __str__(self):
         return self.name
 
@@ -24,7 +25,7 @@ class Category(models.Model):
         return self.name
 
 class Vendor(models.Model):
-    name = models.CharField(max_length = 20)
+    name = models.CharField(max_length = 20, unique=True)
     address = models.TextField()
     email = models.EmailField()
     phone = models.CharField(max_length = 11)
---
+++ rental/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from equipment.models import Equipment
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Rental(models.Model):
+    job = models.ForeignKey('Job', on_delete = models.SET_NULL, null=True)
+    equipment = models.OneToOneField(Equipment, on_delete = models.SET_NULL, null=True)
+    # equipment = models.ForeignKey(Equipment, on_delete = models.SET_NULL, null=True)
+
+    entered_date = models.DateTimeField(auto_now = True)
+    received_date = models.DateTimeField(null = True, blank = True)
+    return_date = models.DateTimeField(null=True, blank=True)
+    buy_or_rent = models.CharField(max_length = 4, choices = [('Buy', 'Buy'), ('Rent', 'Rent')])
+    comments = models.TextField(max_length = 140)
+    entered_via = models.CharField(max_length = 2,
+                                   choices = [('O', 'Order'),('I','Invoice'),('R','Rental Slip')])
+
+
+class Job(models.Model):
+    name = models.CharField(max_length = 20, unique=True)
+    project_manager = models.ForeignKey(User, on_delete = models.SET_NULL, null=True,
+                                        related_name='project_manager')
+    site = models.CharField(max_length = 30)
+    active = models.BooleanField(default = True)
+    supervisor = models.OneToOneField(User, on_delete = models.SET_NULL, null=True,
+                                   related_name='supervisor')
+    PO_num = models.CharField(max_length = 10)
+    def __str__(self):
+        return self.name
+
+
